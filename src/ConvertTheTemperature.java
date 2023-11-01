0b8014d300cd10610e0c9d9b5da24477685daeaf
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-01 20:04:16 +0000

    Solved 2469 Convert the Temperature

--- src/ConvertTheTemperature.java
+++ src/ConvertTheTemperature.java
@@ -1,9 +1,35 @@
 
+// 2469. Convert the Temperature
+
 public class ConvertTheTemperature {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+		
+		Solution solution = new Solution();
+		
+		// Example celsiusArray (you should use your actual data)
+        double celsius = 122.11;
 
+        // Call the convertTemperature method and store the result
+        double[] result = solution.convertTemperature(celsius);
+        
+        
+        // Print the result
+        for (double temperature : result) {
+        	System.out.println(temperature);
+        }
 	}
 
+}
+
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        
+        double kelvin = celsius + 273.15;
+        double farenheit = (celsius * 1.8) + 32;
+
+        double[] ans = {kelvin, farenheit};
+        
+        return ans;
+    }
 }
