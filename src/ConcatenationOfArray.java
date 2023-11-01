ba2322a0abd0fd4d70b8caed4a5dc095f14d4bd6
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-01 20:20:28 +0000

    completed Concatenation of Array

---
+++ src/ConcatenationOfArray.java
@@ -0,0 +1,34 @@
+
+public class ConcatenationOfArray {
+
+	public static void main(String[] args) {
+		
+		class Solution {
+		    public int[] getConcatenation(int[] nums) {
+		        int n = nums.length;
+		        int[] ans = new int[2*n];
+		        for (int i = 0; i < n; i++){
+		            ans[i] = nums[i];
+		            ans[i + n] = nums[i];
+		        }
+
+		        return ans;
+
+		    }
+		}
+		
+		Solution solution = new Solution();
+
+		// Example input array
+        int[] nums = {1, 2};
+
+        // Call the getConcatenation method and store the result
+        int[] concatenatedArray = solution.getConcatenation(nums);
+
+        // Print the result
+        for (int num : concatenatedArray) {
+            System.out.print(num + " ");
+        }
+	}
+
+}
